65fd8e3e8143ac28b99164fbdf9d0ee7bd6b9fe4
Mr_Onion <142563755+SuperUseryjh@users.noreply.github.com>
2025-10-08 19:01:54 +0800

    Feat: 细化显示网页匹配

--- fetch_tampermonkey.user.js
+++ fetch_tampermonkey.user.js
@@ -6,9 +6,10 @@
 // @description  从 OJ 平台获取题目样例并发送到 OICPP
 // @author       Mr_Onion & mywwzh
 // @match        https://www.luogu.com.cn/problem/*
-// @match        https://htoj.com.cn/cpp/oj/problem/detail/*
-// @match        https://atcoder.jp/*
-// @match        https://codeforces.com/*
+// @match        https://htoj.com.cn/cpp/oj/problem/detail?pid=*
+// @match        https://atcoder.jp/contests/*/tasks/*
+// @match        https://codeforces.com/contest/*/problem/*
+// @match        https://codeforces.com/problemset/problem/*/*
 // @grant        GM_xmlhttpRequest
 // @connect      http://127.0.0.1:20030
 // @connect      127.0.0.1
