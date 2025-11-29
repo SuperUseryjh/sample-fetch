01a22213cb6158b5871afd0deb919947bf111152
Mr_Onion <142563755+SuperUseryjh@users.noreply.github.com>
2025-11-29 22:32:50 +0800

    hotfix:修复版本问题

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OICPP sampleTester
 // @namespace    https://oicpp.mywwzh.top/
-// @version      1.0.4-alpha2
+// @version      1.0.4-alpha3
 // @description  从 OJ 平台获取题目样例并发送到 OICPP
 // @author       Mr_Onion & mywwzh
 // @match        https://www.luogu.com.cn/problem/*
